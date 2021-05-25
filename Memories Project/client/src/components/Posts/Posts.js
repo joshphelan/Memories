dc674880aeeec60515aa057d1bd99f799a4bde0b
joshphelan <84751848+joshphelan@users.noreply.github.com>
2021-05-24 20:03:54 -0400

    Initial Commit

---
+++ Memories Project/client/src/components/Posts/Posts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+
+    console.log(posts);
+
+    return (
+        <React.Fragment>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
+        </React.Fragment>
+    );
+}
+
+export default Posts;
